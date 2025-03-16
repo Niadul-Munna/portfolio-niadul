daf406e3c5e5095de774aa31011e62331248cbfa
niadulMunna <niadulislammunna@gmail.com>
2025-03-16 23:31:31 +0600

    refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,9 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          <nav>
-            <Navbar />
-          </nav>
+          <nav>{/* <Navbar /> */}</nav>
           {children}
         </Providers>
       </body>
